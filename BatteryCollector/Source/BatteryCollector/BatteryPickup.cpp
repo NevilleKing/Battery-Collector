2cbb0c590356ad258f2221dc654b8a8e6dd07847
Neville King <nevster07@gmail.com>
2016-10-14 16:40:31 +0100

    Completed Tutorial 11
    Completed "Adding Power to the Game"

--- BatteryCollector/Source/BatteryCollector/BatteryPickup.cpp
+++ BatteryCollector/Source/BatteryCollector/BatteryPickup.cpp
@@ -7,6 +7,9 @@
 ABatteryPickup::ABatteryPickup()
 {
 	GetMesh()->SetSimulatePhysics(true);
+
+	// The base power level of the battery
+	BatteryPower = 150.f;
 }
 
 void ABatteryPickup::WasCollected_Implementation()
@@ -16,5 +19,11 @@ void ABatteryPickup::WasCollected_Implementation()
 
 	// Destroy the battery
 	Destroy();
+}
+
+// report the power level of the battery
+float ABatteryPickup::GetPower()
+{
+	return BatteryPower;
 }
 
