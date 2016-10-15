e68c0786cbcb881c967ce1e593b70f5bc593b4fc
Neville King <nevster07@gmail.com>
2016-10-15 14:45:47 +0100

    Completed Tutorial 14
    Completed "Changing the Character's Speed and Material"

--- BatteryCollector/Source/BatteryCollector/BatteryCollectorCharacter.cpp
+++ BatteryCollector/Source/BatteryCollector/BatteryCollectorCharacter.cpp
@@ -51,6 +51,10 @@ ABatteryCollectorCharacter::ABatteryCollectorCharacter()
 	// Set a base power level for the character
 	InitialPower = 2000.f;
 	CharacterPower = InitialPower;
+
+	// set the dependence of the speed on the power level
+	SpeedFactor = 0.75f;
+	BaseSpeed = 10.0f;
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -197,7 +201,13 @@ float ABatteryCollectorCharacter::GetCurrentPower()
 	return CharacterPower;
 }
 
+// Called whenever power is increased or decreased
 void ABatteryCollectorCharacter::UpdatePower(float PowerChange)
 {
+	// change power
 	CharacterPower = CharacterPower + PowerChange;
+	// change speed based on power
+	GetCharacterMovement()->MaxWalkSpeed = BaseSpeed + SpeedFactor * CharacterPower;
+	// call visual effect
+	PowerChangeEffect();
 }
